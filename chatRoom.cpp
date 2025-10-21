83ffb6b4f4d24156a23662b97a601155cd16c155
Piyush-Singh1234 <shashimasingh755@gmail.com>
2025-10-21 15:01:44 +0530

    updated chatRoom.cpp

--- chatRoom.cpp
+++ chatRoom.cpp
@@ -1,42 +1,46 @@
 #include "chatRoom.hpp"
 
+using boost::asio::ip::tcp;
+using boost::asio::ip::address_v4;
 
-void Room::join(ParticipantPointer participant){
-    this->participants.insert(participant);
-}
-
-void Room::leave(ParticipantPointer participant){
-    this->participants.erase(participant);
-}
-
-void Room::deliver(ParticipantPointer participant, Message &message){
-    messageQueue.push_back(message);
-    while (!messageQueue.empty()) {
-        Message msg = messageQueue.front();
-        messageQueue.pop_front(); 
-        
-        for (ParticipantPointer _participant : participants) {
-            if (participant != _participant) {
-                _participant->write(msg);
-            }
+// ================== Accept Connection ==================
+void accept_connection(boost::asio::io_context &io,char *port,tcp::acceptor &acceptor,Room &room,const tcp::endpoint& endpoint)
+{
+    auto handle_accept = [&](boost::system::error_code ec, tcp::socket socket) {
+        if (!ec) {
+            auto session = std::make_shared<Session>(std::move(socket), room); // Create a new session for this client
+            session->start();
         }
-    }
+        accept_connection(io, port, acceptor, room, endpoint);
+    };
+
+    acceptor.async_accept(handle_accept);
 }
 
+// ================== Session Class ==================
+Session::Session(tcp::socket s, Room& r)
+    : clientSocket(std::move(s)), room(r) {}
+
+void Session::start() {
+    room.join(shared_from_this()); // Join the room
+    async_read();
+}
+
+// ----- Async Read -----
 void Session::async_read() {
     auto self(shared_from_this());
 
     auto handle_read = [this, self](boost::system::error_code ec, std::size_t bytes_transferred) {
         if (!ec) {
-            std::string data(boost::asio::buffers_begin(buffer.data()), 
-                             boost::asio::buffers_begin(buffer.data()) + bytes_transferred);
+            std::string data(boost::asio::buffers_begin(buffer.data()), boost::asio::buffers_begin(buffer.data()) + bytes_transferred);
             buffer.consume(bytes_transferred);
             std::cout << "Received: " << data << std::endl;
 
             Message message(data);
             deliver(message);
             async_read(); 
-        } else {
+        } 
+        else {
             room.leave(shared_from_this());
             if (ec == boost::asio::error::eof) {
                 std::cout << "Connection closed by peer" << std::endl;
@@ -49,76 +53,88 @@ void Session::async_read() {
     boost::asio::async_read_until(clientSocket, buffer, "\n", handle_read);
 }
 
-
-
-void Session::async_write(std::string messageBody, size_t messageLength){   // server to client
-    auto write_handler = [&](boost::system::error_code ec, std::size_t bytes_transferred){
-        if(!ec){
-            std::cout<<"Data is written to the socket: "<<std::endl;
-        }else{
-            std::cerr << "Write error: " << ec.message() << std::endl;
-        }
-    };
-    boost::asio::async_write(clientSocket, boost::asio::buffer(messageBody, messageLength), write_handler);
-}
-void Session::start(){
-    room.join(shared_from_this());
-    async_read();
+// ----- Deliver to Room -----
+void Session::deliver(Message& incomingMessage){
+    room.deliver(shared_from_this(), incomingMessage);
 }
 
-Session::Session(tcp::socket s, Room& r): clientSocket(std::move(s)), room(r){};
-
+// ----- Write to Client -----
 void Session::write(Message &message){
     messageQueue.push_back(message);
-    while(messageQueue.size() != 0){
+    while(!messageQueue.empty()){
         Message message = messageQueue.front();
         messageQueue.pop_front();
+
         bool header_decode = message.decodeHeader();
         if(header_decode){
-            std::string body = message.getBody(); 
+            std::string body = message.getBody();
             async_write(body, message.getBodyLength());
-        }else{
-            std::cout<<"Message length exceeds the max length"<<std::endl;
+        } else {
+            std::cout << "Message length exceeds the max length" << std::endl;
         }
     }
 }
 
-void Session::deliver(Message& incomingMessage){
-    room.deliver(shared_from_this(), incomingMessage);
-}  
-using boost::asio::ip::address_v4;
-
-
-void accept_connection(boost::asio::io_context &io,char *port,tcp::acceptor &acceptor,Room &room,const tcp::endpoint& endpoint)
-{
-    auto handle_accept = [&](boost::system::error_code ec, tcp::socket socket) {
-        if (!ec) {
-            auto session = std::make_shared<Session>(std::move(socket), room);
-            session->start();
+// ----- Async Write -----
+void Session::async_write(std::string messageBody, size_t messageLength){
+    auto write_handler = [&](boost::system::error_code ec, std::size_t bytes_transferred){
+        if(!ec){
+            std::cout << "Data written to the socket" << std::endl;
+        } else {
+            std::cerr << "Write error: " << ec.message() << std::endl;
         }
-        accept_connection(io, port, acceptor, room, endpoint);
     };
 
-    acceptor.async_accept(handle_accept); // callback will be called after client connects 
+    boost::asio::async_write(clientSocket,boost::asio::buffer(messageBody, messageLength),write_handler);
 }
 
+// ================== Room Class ==================
+void Room::join(ParticipantPointer participant){
+    participants.insert(participant);
+}
 
+void Room::leave(ParticipantPointer participant){
+    participants.erase(participant);
+}
 
+// Two-parameter deliver
+void Room::deliver(ParticipantPointer participant, Message &message){
+    messageQueue.push_back(message);
+    while(!messageQueue.empty()){
+        Message msg = messageQueue.front();
+        messageQueue.pop_front(); 
+        
+        for (ParticipantPointer _participant : participants) {
+            if (participant != _participant) {
+                _participant->write(msg);
+            }
+        }
+    }
+}
+
+// ================== Main ==================
 int main(int argc, char *argv[]) {
     try {
         if(argc < 2) {
-            std::cerr << "Usage: server <port> [<port> ...]\n";
+            std::cerr << "Usage: server <port>\n";
             return 1;
         }
+
         Room room;
         boost::asio::io_context io_context;
-        tcp::endpoint endpoint(tcp::v4(), atoi(argv[1]));
-        tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), std::atoi(argv[1])));
+
+        tcp::endpoint endpoint(tcp::v4(), std::atoi(argv[1]));
+        tcp::acceptor acceptor(io_context, endpoint);
+
+        // Start accepting clients
         accept_connection(io_context, argv[1], acceptor, room, endpoint);
+
+        // Run the IO event loop
         io_context.run();
     }
     catch (std::exception& e) {
         std::cerr << "Exception: " << e.what() << "\n";
     }
+
     return 0;
 }
