e88fc95c819e9bf58ef5f7d4d0c341af5ea5abf8
Piyush-Singh1234 <shashimasingh755@gmail.com>
2025-10-21 17:28:31 +0530

    changes logic of Room's deliver

--- chatRoom.cpp
+++ chatRoom.cpp
@@ -99,22 +99,11 @@ void Room::leave(ParticipantPointer participant){
 
 
 void Room::deliver(ParticipantPointer participant, Message &message){
-    // messageQueue.push_back(message);
-    // while(!messageQueue.empty()){
-    //     Message msg = messageQueue.front();
-    //     messageQueue.pop_front(); 
-        
-    //     for (ParticipantPointer _participant : participants) {
-    //         if (participant != _participant) {
-    //             _participant->write(msg);
-    //         }
-    //     }
-    // }
-    for (ParticipantPointer _participant : participants) {
-        if (participant != _participant) {
-            _participant->write(message);
+        for (ParticipantPointer _participant : participants) {
+            if (participant != _participant) {
+                _participant->write(message);
+            }
         }
-    }
 }
 
 // ================== Main ==================
